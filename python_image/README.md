2fd7bf2f2f375a2baf4a6d66496ef8a1667ea643
kristaum <christopher.kristao@gmail.com>
2018-09-27 15:36:23 -0300

    Added files for python image

---
+++ python_image/README.md
@@ -0,0 +1,2 @@
+# Python image
+Here we will create a debian image, add into it the code and install the necessary modules to run the API.
---
+++ python_image/authentication.py
@@ -0,0 +1,5 @@
+# Get your keys and tokens from twitter developer account
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
